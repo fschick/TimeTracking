cd63714f98439409a5a3ae1ac5e84d403871ecd5
Florian Schick <florian.schick@schick-software.de>
2021-02-26 09:44:03 +0100

    Specific message for conflicts on delete added.

--- FS.TimeTracking.UI.Angular/src/app/shared/services/error-handling/api-error.interceptor.ts
+++ FS.TimeTracking.UI.Angular/src/app/shared/services/error-handling/api-error.interceptor.ts
@@ -1,8 +1,9 @@
 import {Injectable} from '@angular/core';
-import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
+import {HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {tap} from 'rxjs/operators';
 import {ToastrService} from 'ngx-toastr';
+import {DatabaseErrorCode, ErrorInformation} from '../api';
 
 @Injectable()
 export class ApiErrorInterceptor implements HttpInterceptor {
@@ -12,9 +13,25 @@ export class ApiErrorInterceptor implements HttpInterceptor {
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     return next.handle(request).pipe(tap(
       () => {},
-      error => {
-        const requestId = error.headers.get('Request-Id');
-        const message = $localize`:@@API.InternalServerError:\r\n` + '.\r\nRequestID:' + requestId;
+      (response: HttpErrorResponse) => {
+
+        let message: string;
+        const error = response.error as ErrorInformation;
+        const requestId = response.headers.get('Request-Id');
+
+        switch (response.status) {
+          case 409:
+            if (request.method === 'DELETE' && error.databaseErrorCode === DatabaseErrorCode.foreignKeyViolation)
+              message = $localize`:@@API.Delete.ForeignKeyViolation:`;
+            else
+              message = $localize`:@@API.Conflict:`;
+            break;
+          default:
+            message = $localize`:@@API.InternalServerError:`;
+        }
+
+        // message += `. RequestID: ${requestId}`;
+
         console.error(message);
         this.toastrService.error(message);
       }));
