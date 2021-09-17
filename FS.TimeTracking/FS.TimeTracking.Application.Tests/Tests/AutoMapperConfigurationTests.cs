13d7e1f1bdbe59d34a4245beffc783ced01e433d
Florian Schick <florian.schick@schick-software.de>
2021-09-17 21:34:47 +0200

    Auto mapper configuration tests fixed.

--- FS.TimeTracking/FS.TimeTracking.Application.Tests/Tests/AutoMapperConfigurationTests.cs
+++ FS.TimeTracking/FS.TimeTracking.Application.Tests/Tests/AutoMapperConfigurationTests.cs
@@ -1,11 +1,6 @@
 ﻿using AutoMapper;
-using AutoMapper.Configuration;
-using FluentAssertions;
-using FluentAssertions.Execution;
 using FS.TimeTracking.Application.AutoMapper;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
-using System.Linq;
 
 namespace FS.TimeTracking.Application.Tests.Tests
 {
@@ -15,16 +10,10 @@ namespace FS.TimeTracking.Application.Tests.Tests
         [TestMethod]
         public void AllMappingConfigurations_ShouldBeValid()
         {
-            var timeTrackingConfiguration = (IProfileConfiguration)new TimeTrackingAutoMapper();
-            var mappingValidations = timeTrackingConfiguration
-                .TypeMapConfigs
-                .Select(x => new MapperConfiguration(cc => cc.CreateMap(x.SourceType, x.DestinationType)))
-                .Select(mapping => (Action)mapping.AssertConfigurationIsValid)
-                .ToList();
-
-            using (new AssertionScope())
-                foreach (var mappingValidation in mappingValidations)
-                    mappingValidation.Should().NotThrow();
+            new MapperConfiguration(cfg => cfg.AddProfile<TimeTrackingAutoMapper>())
+                .CreateMapper()
+                .ConfigurationProvider
+                .AssertConfigurationIsValid();
         }
     }
 }
