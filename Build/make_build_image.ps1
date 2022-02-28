9bc9636aaa164e8f58674676cbab44b71f0bfa39
Florian Schick <florian.schick@schick-software.de>
2022-02-28 21:35:27 +0100

    Publish to docker container added.

--- Build/make_build_image.ps1
+++
@@ -1,1 +0,0 @@
-& docker build -t schicksoftware/dotnet6.0-jre17-nodejs16.0 -f build_image.docker .
---
+++ Build/make_docker_sdk.ps1
@@ -0,0 +1,1 @@
+& docker build -t schicksoftware/dotnet6.0-jre17-nodejs16.0 -f make_docker_sdk.docker .
