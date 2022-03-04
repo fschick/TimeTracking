821df7c312d9056ad11dfb4f044402fc6c4cb791
Florian Schick <florian.schick@schick-software.de>
2022-03-04 13:53:13 +0100

    Display of worked days overview optimized.

--- FS.TimeTracking.UI.Angular/cypress/integration/timesheet/components/timesheet/timesheet.component.tests.ts
+++ FS.TimeTracking.UI.Angular/cypress/integration/timesheet/components/timesheet/timesheet.component.tests.ts
@@ -1,13 +1,21 @@
 ﻿describe('TimeSheet table', () => {
   beforeEach(() => {
     cy.intercept('GET', '/api/v1/TimeSheet/GetListFiltered**', {fixture: 'dto/TimeSheetListDto.single.json'}).as('default');
-    cy.intercept('GET', '/api/v1/Workday/GetWorkedDaysInfo**', {fixture: 'dto/WorkedTimeInfoDto.single.json'}).as('default');
   })
 
-  it('Caption is displayed with mathcing spaces', () => {
+  it('Caption is displayed with matching spaces (no holidays)', () => {
+    cy.intercept('GET', '/api/v1/Workday/GetWorkedDaysInfo**', {fixture: 'dto/WorkedTimeInfoDto.single.json'}).as('default');
     cy.visit('/');
 
     cy.get('#timeSheetTableCaption')
-      .should('have.text', "Timesheet (0.8 days worked,  1 workdays,  0 holidays) ");
+      .should('have.text', "Timesheet (0.8 days worked,  1 working days) ");
+  })
+
+  it('Caption is displayed with matching spaces (with holidays)', () => {
+    cy.intercept('GET', '/api/v1/Workday/GetWorkedDaysInfo**', {fixture: 'dto/WorkedTimeInfoDto.holidays.json'}).as('default');
+    cy.visit('/');
+
+    cy.get('#timeSheetTableCaption')
+      .should('have.text', "Timesheet (0.8 days worked,  1 working days,  1 vacation days) ");
   })
 })
