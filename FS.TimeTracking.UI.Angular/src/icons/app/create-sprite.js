790f70e8aa856487126f69b6a06af28132b80d11
Florian Schick <florian.schick@schick-software.de>
2022-03-19 17:46:08 +0100

    Generation of custom bootstrap icon sprite added.

---
+++ FS.TimeTracking.UI.Angular/src/icons/app/create-sprite.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const { exec } = require('child_process');
+
+const configFile = process.argv[2];
+const destDirectory = process.argv[3];
+const fileList = process.argv[4];
+const files = fs.readFileSync(fileList,'utf8').replace(/(\r|\n|\r\n)/g, ' ');
+const command = `npx svg-sprite --config ${configFile} --log=info --dest=${destDirectory}`;
+
+exec(`${command} ${files}`).stdout.pipe(process.stdout);
