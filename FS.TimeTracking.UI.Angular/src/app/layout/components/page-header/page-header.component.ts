6bf1bd15c8de58c54942e2149059dca0cf398446
Florian Schick <florian.schick@schick-software.de>
2022-03-26 21:12:43 +0100

    Page header added.

---
+++ FS.TimeTracking.UI.Angular/src/app/layout/components/page-header/page-header.component.ts
@@ -0,0 +1,12 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'ts-page-header',
+  templateUrl: './page-header.component.html',
+  styleUrls: ['./page-header.component.scss']
+})
+export class PageHeaderComponent {
+
+  constructor() { }
+
+}
