2cec506bd272562551e25744dfc9232844961578
Florian Schick <florian.schick@schick-software.de>
2022-04-03 21:40:16 +0200

    Last work day/week/month/year info added to WorkedDaysInfoDto.

---
+++ FS.TimeTracking/FS.TimeTracking.Abstractions/DTOs/Shared/WorkdayDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace FS.TimeTracking.Abstractions.DTOs.Shared;
+
+/// <summary>
+/// Last worked times.
+/// </summary>
+public class WorkdayDto
+{
+    /// <summary>
+    /// The date.
+    /// </summary>
+    [Required]
+    public DateTime Date { get; set; }
+
+    /// <summary>
+    /// The time worked.
+    /// </summary>
+    [Required]
+    public TimeSpan TimeWorked { get; set; }
+}
