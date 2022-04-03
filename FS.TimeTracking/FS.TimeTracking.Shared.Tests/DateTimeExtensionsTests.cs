880aeb255a34badce7f6b9423864b55eb27ab0a4
Florian Schick <florian.schick@schick-software.de>
2022-04-03 21:40:16 +0200

    Last work day/week/month/year info added to WorkedDaysInfoDto.

--- FS.TimeTracking/FS.TimeTracking.Shared.Tests/DateTimeExtensionsTests.cs
+++ FS.TimeTracking/FS.TimeTracking.Shared.Tests/DateTimeExtensionsTests.cs
@@ -11,15 +11,49 @@ namespace FS.TimeTracking.Shared.Tests;
 public class DateTimeExtensionsTests
 {
     [DataTestMethod]
-    [DynamicData(nameof(GetStartEndDateTimeRanges), DynamicDataSourceType.Method)]
-    public void WhenDatesBetweenTwoDatesRequested_CountMatchesExpected(DateTime start, DateTime end, int expectedDayCount)
+    [DynamicData(nameof(WhenDaysBetweenTwoDatesRequested_CountMatchesExpectedDays_Data), DynamicDataSourceType.Method)]
+    public void WhenDaysBetweenTwoDatesRequested_CountMatchesExpectedDays(DateTime start, DateTime end, int expectedDayCount)
+        => start.GetDays(end).Should().HaveCount(expectedDayCount);
+
+    public static IEnumerable<object[]> WhenDaysBetweenTwoDatesRequested_CountMatchesExpectedDays_Data()
     {
-        start.GetDays(end).Should().HaveCount(expectedDayCount);
+        yield return new object[] { new DateTime(2000, 1, 1, 12, 0, 0), new DateTime(2000, 1, 1), 1 };
+        yield return new object[] { new DateTime(2000, 1, 1, 12, 0, 0), new DateTime(2000, 1, 31), 31 };
     }
 
-    public static IEnumerable<object[]> GetStartEndDateTimeRanges()
+    [DataTestMethod]
+    [DynamicData(nameof(WhenStartOfYearIsRequested_StartOfYearIsReturned_Data), DynamicDataSourceType.Method)]
+    public void WhenStartOfYearIsRequested_StartOfYearIsReturned(DateTime dateTime, DateTime expected)
+        => dateTime.StartOfYear().Should().Be(expected);
+
+    public static IEnumerable<object[]> WhenStartOfYearIsRequested_StartOfYearIsReturned_Data()
     {
-        yield return new object[] { new DateTime(2000, 1, 1), new DateTime(2000, 1, 1), 1 };
-        yield return new object[] { new DateTime(2000, 1, 1), new DateTime(2000, 1, 31), 31 };
+        yield return new object[] { new DateTime(2000, 1, 1, 12, 0, 0), new DateTime(2000, 1, 1) };
+        yield return new object[] { new DateTime(2000, 3, 31, 12, 0, 0), new DateTime(2000, 1, 1) };
+        yield return new object[] { new DateTime(2000, 12, 31, 12, 0, 0), new DateTime(2000, 1, 1) };
+    }
+
+    [DataTestMethod]
+    [DynamicData(nameof(WhenStartOfMonthIsRequested_StartOfYearIsReturned_Data), DynamicDataSourceType.Method)]
+    public void WhenStartOfMonthIsRequested_StartOfYearIsReturned(DateTime dateTime, DateTime expected)
+        => dateTime.StartOfMonth().Should().Be(expected);
+
+    public static IEnumerable<object[]> WhenStartOfMonthIsRequested_StartOfYearIsReturned_Data()
+    {
+        yield return new object[] { new DateTime(2000, 1, 1, 12, 0, 0), new DateTime(2000, 1, 1) };
+        yield return new object[] { new DateTime(2000, 3, 31, 12, 0, 0), new DateTime(2000, 3, 1) };
+        yield return new object[] { new DateTime(2000, 12, 31, 12, 0, 0), new DateTime(2000, 12, 1) };
+    }
+
+    [DataTestMethod]
+    [DynamicData(nameof(WhenStartOfWeekIsRequested_StartOfYearIsReturned_Data), DynamicDataSourceType.Method)]
+    public void WhenStartOfWeekIsRequested_StartOfYearIsReturned(DateTime dateTime, DateTime expected)
+        => dateTime.StartOfWeek().Should().Be(expected);
+
+    public static IEnumerable<object[]> WhenStartOfWeekIsRequested_StartOfYearIsReturned_Data()
+    {
+        yield return new object[] { new DateTime(2000, 1, 1, 12, 0, 0), new DateTime(1999, 12, 27) };
+        yield return new object[] { new DateTime(2000, 1, 9, 12, 0, 0), new DateTime(2000, 1, 3) };
+        yield return new object[] { new DateTime(2000, 1, 10, 12, 0, 0), new DateTime(2000, 1, 10) };
     }
 }
