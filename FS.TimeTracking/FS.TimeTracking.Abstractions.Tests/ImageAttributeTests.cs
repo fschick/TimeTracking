fe70cb1ff4479ccf949786c385093ba0b9349686
Florian Schick <florian.schick@schick-software.de>
2022-09-05 13:55:50 +0200

    Image validation attribute added.

---
+++ FS.TimeTracking/FS.TimeTracking.Abstractions.Tests/ImageAttributeTests.cs
@@ -0,0 +1,129 @@
+using FluentAssertions;
+using FS.TimeTracking.Abstractions.Attributes.Validation;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace FS.TimeTracking.Abstractions.Tests;
+
+[TestClass, ExcludeFromCodeCoverage]
+public class ImageAttributeTests
+{
+    private static readonly Assembly _resourceAssembly = typeof(ImageAttributeTests).GetTypeInfo().Assembly;
+
+    [DataTestMethod]
+    [DynamicData(nameof(GetTestImages), DynamicDataSourceType.Method)]
+    public void WhenValidImageDataIsValidated_ValidationSucceeds(string imageResourceName)
+    {
+        // Prepare
+        using var resource = _resourceAssembly.GetManifestResourceStream(imageResourceName);
+        if (resource == null)
+            throw new ArgumentException($"Image resource with name {imageResourceName} not found.", nameof(imageResourceName));
+
+        var imageValidation = new ImageValidation { ImageStream = resource, ImageBytes = ReadBytes(resource) };
+
+        // Act
+        var isValid = ValidateImage(imageValidation);
+
+        // Check
+        isValid.Should().BeTrue();
+    }
+
+    [TestMethod]
+    public void WhenNullImageDataIsValidated_ValidationSucceeds()
+    {
+        var nullImageValidation = new ImageValidation { ImageStream = null, ImageBytes = null };
+        var isValid = ValidateImage(nullImageValidation);
+        isValid.Should().BeTrue();
+    }
+
+    [TestMethod]
+    public void WhenEmptyImageDataIsValidated_ValidationSucceeds()
+    {
+        var imageBytes = Array.Empty<byte>();
+        using var imageStream = new MemoryStream(imageBytes);
+        var nullImageValidation = new ImageValidation { ImageStream = imageStream, ImageBytes = imageBytes };
+        var isValid = ValidateImage(nullImageValidation);
+        isValid.Should().BeTrue();
+    }
+
+    [DataTestMethod]
+    [DynamicData(nameof(BadTestImages), DynamicDataSourceType.Method)]
+    public void WhenInvalidImageDataIsValidated_ValidationFails(byte[] badImageBytes)
+    {
+        using var imageStream = new MemoryStream(badImageBytes);
+        var nullImageValidation = new ImageValidation { ImageStream = imageStream, ImageBytes = badImageBytes };
+        var isValid = ValidateImage(nullImageValidation);
+        isValid.Should().BeFalse();
+    }
+
+    [DataTestMethod]
+    [DynamicData(nameof(BigTestImages), DynamicDataSourceType.Method)]
+    public void WhenTooBigImageDataIsValidated_ValidationFails(byte[] bigImageBytes)
+    {
+        using var imageStream = new MemoryStream(bigImageBytes);
+        var nullImageValidation = new ImageValidation { ImageStream = imageStream, ImageBytes = bigImageBytes };
+        var isValid = ValidateImage(nullImageValidation);
+        isValid.Should().BeFalse();
+    }
+
+    [SuppressMessage("ReSharper", "UnusedAutoPropertyAccessor.Local")]
+    private class ImageValidation
+    {
+        [Image(MaxFileSize = 1024, MaxImageHeight = 400, MaxImageWidth = 400)]
+        public Stream ImageStream { get; set; }
+
+        [Image(MaxFileSize = 1024, MaxImageHeight = 400, MaxImageWidth = 400)]
+        public byte[] ImageBytes { get; set; }
+    }
+
+    private static IEnumerable<object[]> GetTestImages()
+        => _resourceAssembly
+            .GetManifestResourceNames()
+            .Where(resourceName => resourceName.Contains("ImageAttributeTestImage"))
+            .Select(imageResourceName => new object[] { imageResourceName });
+
+    private static byte[] ReadBytes(Stream stream)
+    {
+        using var memoryStream = new MemoryStream();
+        stream.Position = 0;
+        stream.CopyTo(memoryStream);
+        return memoryStream.ToArray();
+    }
+
+    private static bool ValidateImage(ImageValidation imageValidation)
+    {
+        var context = new ValidationContext(imageValidation);
+        var validationResults = new List<ValidationResult>();
+        var isValid = Validator.TryValidateObject(imageValidation, context, validationResults, true);
+        return isValid;
+    }
+
+    private static byte[] CreateImage(int width, int height)
+    {
+        using var memoryStream = new MemoryStream();
+        new Image<Abgr32>(width, height).SaveAsPng(memoryStream);
+        return memoryStream.ToArray();
+    }
+
+    private static IEnumerable<object[]> BadTestImages()
+    {
+        yield return new object[] { new byte[] { 0, 0 } }; // Bad at all
+        yield return new object[] { new byte[] { 0x42, 0x4d } }; // PNG without data
+    }
+
+    private static IEnumerable<object[]> BigTestImages()
+    {
+        yield return new object[] { CreateImage(1, 401) };
+        yield return new object[] { CreateImage(401, 1) };
+        yield return new object[] { CreateImage(399, 399) };
+    }
+
+}
---
+++ FS.TimeTracking/FS.TimeTracking.Abstractions/Attributes/Validation/ImageAttribute.cs
@@ -0,0 +1,95 @@
+﻿using FileTypeChecker;
+using SixLabors.ImageSharp;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
+
+namespace FS.TimeTracking.Abstractions.Attributes.Validation;
+
+/// <summary>
+/// Validates an uploaded image.
+/// </summary>
+internal class ImageAttribute : ValidationAttribute
+{
+    private const string IMAGE_STREAM_NOT_SEEKABLE = "Image stream must be seekable / buffered in order to be validated.";
+
+    /// <summary>
+    /// Gets or sets the maximum size of the file/stream in bytes.
+    /// </summary>
+    public long MaxFileSize { get; set; }
+
+    /// <summary>
+    /// Gets or sets the maximum width of the image.
+    /// </summary>
+    public int MaxImageWidth { get; set; }
+
+    /// <summary>
+    /// Gets or sets the maximum height of the image.
+    /// </summary>
+    public int MaxImageHeight { get; set; }
+
+    /// <inheritdoc />
+    protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+    {
+        try
+        {
+            switch (value)
+            {
+                case null:
+                    return ValidationResult.Success;
+                case byte[] bytes:
+                    using (var stream = new MemoryStream(bytes))
+                        return ValidateImageStream(stream, validationContext.DisplayName);
+                case Stream stream:
+                    return ValidateImageStream(stream, validationContext.DisplayName);
+                default:
+                    return new ValidationResult($"Validation of images of type {value.GetType()} is unsupported.");
+            }
+        }
+        catch (UnknownImageFormatException)
+        {
+            return new ValidationResult("Image cannot be loaded: Unknown image format.");
+        }
+        catch (NotSupportedException ex) when (ex.Message.StartsWith("ImageSharp does not support this"))
+        {
+            return new ValidationResult("Image cannot be loaded: Unknown image format.");
+        }
+        catch (NotSupportedException ex) when (ex.Message == IMAGE_STREAM_NOT_SEEKABLE)
+        {
+            return new ValidationResult(ex.Message);
+        }
+        catch
+        {
+            return new ValidationResult("Unknown error occurred.");
+        }
+    }
+
+    private ValidationResult ValidateImageStream(Stream imageStream, string memberName)
+    {
+        if (MaxFileSize != 0 && imageStream.Length > MaxFileSize)
+            return new ValidationResult($"{memberName} exceeds max file/stream length.");
+
+        if (!imageStream.CanSeek)
+            throw new NotSupportedException(IMAGE_STREAM_NOT_SEEKABLE);
+
+        imageStream.Position = 0;
+        using var memoryStream = new MemoryStream();
+        imageStream.CopyTo(memoryStream);
+        if (memoryStream.Length == 0)
+            return ValidationResult.Success;
+
+        imageStream.Position = 0;
+        if (!FileTypeValidator.IsImage(imageStream))
+            return new ValidationResult($"{memberName} not recognized as image.");
+
+        imageStream.Position = 0;
+        using var image = Image.Load(imageStream);
+        if (MaxImageWidth != 0 && image.Width > MaxImageWidth)
+            return new ValidationResult($"{memberName} exceeds max image width.");
+
+        if (MaxImageHeight != 0 && image.Height > MaxImageHeight)
+            return new ValidationResult($"{memberName} exceeds max image height.");
+
+        return ValidationResult.Success;
+    }
+}
