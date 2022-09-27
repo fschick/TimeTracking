4ae292183de59ae4fa457b96a6b7742cf5289e2f
Florian Schick <florian.schick@schick-software.de>
2022-09-27 21:13:09 +0200

    Tests for IssueChartService.GetWorkTimesPerIssue(...) added.

--- FS.TimeTracking/FS.TimeTracking.Application.Tests/Services/FakeTimeSheet.cs
+++ FS.TimeTracking/FS.TimeTracking.Application.Tests/Services/FakeTimeSheet.cs
@@ -9,7 +9,7 @@ namespace FS.TimeTracking.Application.Tests.Services;
 [ExcludeFromCodeCoverage]
 public static class FakeTimeSheet
 {
-    public static TimeSheet Create(Guid projectId, Guid activityId, Guid? orderId = null, DateTimeOffset? startDate = null, DateTimeOffset? endDate = null, string prefix = "Test")
+    public static TimeSheet Create(Guid projectId, Guid activityId, Guid? orderId = null, DateTimeOffset? startDate = null, DateTimeOffset? endDate = null, string issue = null, string prefix = "Test")
         => new()
         {
             Id = Guid.NewGuid(),
@@ -20,11 +20,12 @@ public static class FakeTimeSheet
             EndDate = endDate ?? (startDate == null ? DateTimeOffset.Now.Date.AddHours(12) : null),
             Billable = true,
             Comment = $"{prefix}{nameof(TimeSheet.Comment)}",
+            Issue = issue ?? $"{prefix}{nameof(TimeSheet.Issue)}",
         };
 
-    public static TimeSheet Create(Project project, Activity activity, Order order = null, DateTimeOffset? startDate = null, DateTimeOffset? endDate = null, string prefix = "Test")
+    public static TimeSheet Create(Project project, Activity activity, Order order = null, DateTimeOffset? startDate = null, DateTimeOffset? endDate = null, string issue = null, string prefix = "Test")
     {
-        var timeSheet = Create(project.Id, activity.Id, order?.Id, startDate, endDate, prefix);
+        var timeSheet = Create(project.Id, activity.Id, order?.Id, startDate, endDate, issue, prefix);
         timeSheet.Project = project;
         timeSheet.Activity = activity;
         timeSheet.Order = order;
---
+++ FS.TimeTracking/FS.TimeTracking.Application.Tests/Tests/Services/IssueChartServiceTests.cs
@@ -0,0 +1,109 @@
+﻿using Autofac.Extras.FakeItEasy;
+using FluentAssertions;
+using FluentAssertions.Execution;
+using FS.TimeTracking.Application.Services.Chart;
+using FS.TimeTracking.Application.Services.Shared;
+using FS.TimeTracking.Application.Tests.Attributes;
+using FS.TimeTracking.Application.Tests.Extensions;
+using FS.TimeTracking.Application.Tests.Models;
+using FS.TimeTracking.Application.Tests.Services;
+using FS.TimeTracking.Core.Interfaces.Application.Services.Chart;
+using FS.TimeTracking.Core.Interfaces.Application.Services.Shared;
+using FS.TimeTracking.Core.Interfaces.Repository.Services;
+using FS.TimeTracking.Core.Models.Application.MasterData;
+using FS.TimeTracking.Core.Models.Application.TimeTracking;
+using FS.TimeTracking.Repository.DbContexts;
+using FS.TimeTracking.Repository.Services;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Threading.Tasks;
+
+namespace FS.TimeTracking.Application.Tests.Tests.Services;
+
+[TestClass, ExcludeFromCodeCoverage]
+public class IssueChartServiceTests
+{
+    [DataTestMethod]
+    [WorkTimesPerIssueDataSource]
+    public async Task WhenGetWorkTimesPerIssueRequested_ResultMatchesExpectedValues(WorkTimesPerIssueTestCase testCase)
+    {
+        // Prepare
+        using var autoFake = new AutoFake();
+
+        await autoFake.ConfigureInMemoryDatabase();
+        autoFake.Provide(FakeAutoMapper.Mapper);
+        autoFake.Provide<IRepository, Repository<TimeTrackingDbContext>>();
+        autoFake.Provide<IWorkdayService, WorkdayService>();
+        autoFake.Provide<IIssueChartService, IssueChartService>();
+
+        var repository = autoFake.Resolve<IRepository>();
+        await repository.AddRange(testCase.TimeSheets.EliminateDuplicateReferences());
+        await repository.SaveChanges();
+
+        // Act
+        var issueChartService = autoFake.Resolve<IIssueChartService>();
+        var workTimePerIssue = await issueChartService.GetWorkTimesPerIssue(FakeFilters.Empty());
+
+        // Check
+        using var _ = new AssertionScope();
+        workTimePerIssue.Should().HaveSameCount(testCase.Expected);
+        workTimePerIssue.Should().BeEquivalentTo(testCase.Expected, options => options.WithStrictOrdering());
+    }
+}
+
+public class WorkTimesPerIssueDataSourceAttribute : TestCaseDataSourceAttribute
+{
+    public WorkTimesPerIssueDataSourceAttribute() : base(GetTestCases()) { }
+
+    private static List<TestCase> GetTestCases()
+    {
+        var customer = FakeCustomer.Create();
+        var project = FakeProject.Create(customer);
+        var activity = FakeActivity.Create(project);
+
+        return new List<TestCase>
+        {
+            new WorkTimesPerIssueTestCase
+            {
+                Identifier = "Two_issues_2_and_1_third",
+                TimeSheets = new List<TimeSheet> {
+                    CreateTimeSheet(project, activity, "IssueA"),
+                    CreateTimeSheet(project, activity, "IssueA"),
+                    CreateTimeSheet(project, activity, "IssueB"),
+                },
+                Expected = new List<object>
+                {
+                    new { Issue = "IssueA", TimeWorked = TimeSpan.FromHours(2) },
+                    new { Issue = "IssueB", TimeWorked = TimeSpan.FromHours(1) },
+                },
+            },
+            new WorkTimesPerIssueTestCase
+            {
+                Identifier = "No_issues",
+                TimeSheets = new List<TimeSheet> {
+                    CreateTimeSheet(project, activity, string.Empty),
+                    CreateTimeSheet(project, activity, string.Empty),
+                },
+                Expected = new List<object> {
+                    new { Issue = string.Empty, TimeWorked = TimeSpan.FromHours(2) }
+                },
+            },
+            new WorkTimesPerIssueTestCase
+            {
+                Identifier = "No_time_sheets",
+                Expected = new List<object>(),
+            },
+        };
+    }
+
+    private static TimeSheet CreateTimeSheet(Project project, Activity activity, string issue)
+        => FakeTimeSheet.Create(project, activity, null, FakeDateTime.Offset("2020-06-01 03:00"), FakeDateTime.Offset("2020-06-01 04:00"), issue: issue);
+}
+
+public class WorkTimesPerIssueTestCase : TestCase
+{
+    public List<TimeSheet> TimeSheets { get; set; } = new();
+    public List<object> Expected { get; set; } = new();
+}
