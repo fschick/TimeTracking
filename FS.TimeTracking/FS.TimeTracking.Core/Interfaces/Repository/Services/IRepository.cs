a0e6d841c6e526b5866641d1056f234852465603
Florian Schick <florian.schick@schick-software.de>
2022-10-23 10:43:38 +0200

    Check source database version to match code version before import.

--- FS.TimeTracking/FS.TimeTracking.Core/Interfaces/Repository/Services/IRepository.cs
+++ FS.TimeTracking/FS.TimeTracking.Core/Interfaces/Repository/Services/IRepository.cs
@@ -270,4 +270,16 @@ public interface IRepository
     /// <param name="cancellationToken">A <see cref="CancellationToken" /> to observe while waiting for the task to complete.</param>
     /// <returns>A task that represents the asynchronous save operation. The task result contains the number of state entries written to the database.</returns>
     Task<int> SaveChanges(CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Gets all the migrations that are defined in the configured migrations assembly.
+    /// </summary>
+    /// <param name="cancellationToken">a token that allows processing to be cancelled.</param>
+    Task<IEnumerable<string>> GetMigrations(CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Gets all migrations that have been applied to the target database.
+    /// </summary>
+    /// <param name="cancellationToken"> a token that allows processing to be cancelled.</param>
+    Task<IEnumerable<string>> GetAppliedMigrations(CancellationToken cancellationToken = default);
 }
