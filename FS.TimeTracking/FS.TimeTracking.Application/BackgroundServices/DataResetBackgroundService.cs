6f92503eaca1ce613c9c34bdaf563c299e5a0fa2
Florian Schick <florian.schick@schick-software.de>
2023-05-20 14:00:56 +0200

    Background service to reset database added.

---
+++ FS.TimeTracking/FS.TimeTracking.Application/BackgroundServices/DataResetBackgroundService.cs
@@ -0,0 +1,32 @@
+﻿using FS.TimeTracking.Core.Interfaces.Application.Services.Administration;
+using FS.TimeTracking.Core.Models.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FS.TimeTracking.Application.BackgroundServices;
+
+internal class DataResetBackgroundService : CronBackgroundService
+{
+    private readonly IServiceScopeFactory _serviceScopeFactory;
+
+    // ReSharper disable once SuggestBaseTypeForParameterInConstructor
+    //  Justification: Logger should contain class type as category.
+    public DataResetBackgroundService(ILogger<DataResetBackgroundService> logger, IServiceScopeFactory serviceScopeFactory, IOptions<TimeTrackingConfiguration> configuration)
+        : base(logger, "data-reset", GetCronJobSchedule(configuration.Value))
+        => _serviceScopeFactory = serviceScopeFactory;
+
+    protected override async Task NextExecuteAsync(CancellationToken cancellationToken)
+    {
+        using var scope = _serviceScopeFactory.CreateScope();
+        var maintenanceService = scope.ServiceProvider.GetRequiredService<IMaintenanceService>();
+        await maintenanceService.ResetDatabase();
+    }
+
+    private static string GetCronJobSchedule(TimeTrackingConfiguration configuration)
+        => configuration.DataReset.Enabled
+            ? configuration.DataReset.CronJobSchedule
+            : null;
+}
--- FS.TimeTracking/FS.TimeTracking.Repository/Services/DbRepository.cs
+++ FS.TimeTracking/FS.TimeTracking.Repository/Services/DbRepository.cs
@@ -131,6 +131,24 @@ public class DbRepository<TDbContext> : IDbRepository where TDbContext : DbConte
             .SumAsyncEF(select, cancellationToken);
 
     /// <inheritdoc />
+    public async Task<TResult> Min<TEntity, TResult>(
+        Expression<Func<TEntity, TResult>> select,
+        Expression<Func<TEntity, bool>> where = null,
+        CancellationToken cancellationToken = default
+    ) where TEntity : class
+        => await GetInternal(x => x, where, null, null, null, false, null, null, false)
+            .MinAsyncEF(select, cancellationToken);
+
+    /// <inheritdoc />
+    public async Task<TResult> Max<TEntity, TResult>(
+        Expression<Func<TEntity, TResult>> select,
+        Expression<Func<TEntity, bool>> where = null,
+        CancellationToken cancellationToken = default
+    ) where TEntity : class
+        => await GetInternal(x => x, where, null, null, null, false, null, null, false)
+            .MaxAsyncEF(select, cancellationToken);
+
+    /// <inheritdoc />
     public Task<bool> Exists<TEntity>(
         Expression<Func<TEntity, bool>> where = null,
         CancellationToken cancellationToken = default
